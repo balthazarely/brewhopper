53dbb82aaba7563039409fcc5ded0f27a8194bed
balthazarely <balthazar.elyj@gmail.com>
2023-07-10 10:40:48 -0600

    another test

--- brewhopper-frontend/src/App.tsx
+++ brewhopper-frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { ToastContainer } from "react-toastify";
 import "./App.css";
-// import { Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 import { Navbar } from "./components/layout";
 
@@ -9,7 +9,9 @@ function App() {
     <>
       <Navbar />
       <div className="flex flex-col min-h-screen pt-20">
-        <main className="flex-grow  flex  h-full">{/* <Outlet /> */}</main>
+        <main className="flex-grow  flex  h-full">
+          <Outlet />
+        </main>
       </div>
       <ToastContainer position="bottom-right" />
     </>
