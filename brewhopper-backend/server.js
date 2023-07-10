78b7acb1aa09f1d3f8d71a853e96aca781edf723
balthazarely <balthazar.elyj@gmail.com>
2023-07-10 09:59:43 -0600

    dist change

--- brewhopper-backend/server.js
+++ brewhopper-backend/server.js
@@ -61,6 +61,8 @@ if (process.env.NODE_ENV === "production") {
 } else {
   const __dirname = path.resolve();
   // app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+  app.use(express.static(path.join(__dirname, "/brewhopper-frontend/dist")));
+
   app.get("/", (req, res) => {
     res.send("API is running....");
   });
